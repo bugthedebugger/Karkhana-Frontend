a521636e34276759e804cb52dc5dc4d8f489a9af
Bibhuti <bibhutipd@gmail.com>
2020-02-03 21:22:52 +0545

    Update content at About->team section

--- components/AboutTeam.vue
+++ components/AboutTeam.vue
@@ -3,29 +3,20 @@
     <div class="container">
       <div class="row">
         <div class="col">
-          <h1 class="title">Meet The Karkhanaughts</h1>
-          <p class="subtitle">
-            The best part of this crazy journey is the people we work with.
-            We are a diverse team of engineers, designers, social workers, programmers and 
-            educators that are bound together by one vision: empower people with the skills 
-            &amp; attitudes that will help them build their future and the future of their community.
-          </p>
+          <h1 class="title">Meet Our Team</h1>
+          <p class="subtitle">We are passionate about the pursuit of ideas that people put first.</p>
         </div>
       </div>
     </div>
 
     <div class="team-image">
       <div class="team-info">
-        <h1 class="title">The Team</h1>
-        <p class="subtitle">
-          We are a team of full-stack programming experts and designers with
-          surprisingly deep knowledge of knitting and photography.
-        </p>
+        <h1 class="title">Meet The Karkhanaughts</h1>
+        <p class="subtitle">The best part of this crazy journey is the people we work with.</p>
         <p class="content">
-          Some of us are very, very good at chess and we are unanimously bad at yachting.
-          We all work together in a fancy office building. If we are not working together
-          in a aforementioned building, we are very likely to spend time together somewhere else,
-          we are friends after all.
+          We are a diverse team of engineers, designers, social workers, programmers and
+          educators that are bound together by one vision: empower people with the skills
+          &amp; attitudes that will help them build their future and the future of their community.
         </p>
       </div>
     </div>
