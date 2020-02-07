499dfaec59206b656cab43c508a086c77a08c435
Bibhuti <bibhutipd@gmail.com>
2020-02-07 22:07:50 +0545

    Dasgboard designs

---
+++ pages/dashboard/blogs.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="dashboard-content">
+    <div class="toolbar">
+      <h1 class="title">Blog Posts</h1>
+      <div class="toolbar-controls">
+        <form class="form-inline">
+          <div class="input-group mb-2 mr-sm-2 search-field">
+            <div class="input-group-prepend">
+              <div class="input-group-text">@</div>
+            </div>
+            <input type="text" class="form-control search-input" />
+          </div>
+
+          <nuxt-link to="/dashboard/create-blog" class="btn btn-primary mb-2">
+            <i class="fal fa-plus"></i> &nbsp;
+            New Blog Post
+          </nuxt-link>
+        </form>
+      </div>
+    </div>
+
+    <div class="container-card">
+      <div class="menubar"></div>
+      <ul class="dashboard-list no-bullets">
+        <li v-for="blogPost in blogPosts" :key="blogPost.uuid" class="dashboard-list-item d-flex">
+          <label class="checkbox title flex-fill">
+            <input type="checkbox" />
+            {{blogPost.title}}
+          </label>
+          <label class="author">{{blogPost.author}}</label>
+          <label class="created-at">{{blogPost.created_at}}</label>
+          <label class="views">
+            {{blogPost.views}}
+            <i class="fal fa-arrow-up"></i>
+          </label>
+          <div class="controls d-flex">
+            <div class="btn-view">
+              <i class="fal fa-eye"></i>
+            </div>
+            <div class="btn-edit">
+              <i class="fal fa-edit"></i>
+            </div>
+            <div class="btn-delete">
+              <i class="fal fa-trash-alt"></i>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  layout: "dashboard",
+  data() {
+    return {
+      blogPosts: [
+        {
+          uuid: 1,
+          title: "The best way to predict the way is to invent it",
+          author: "Bibhuti Poudyal",
+          created_at: "20/08/2020",
+          views: 108
+        },
+        {
+          uuid: 1,
+          title: "The best way to predict the way is to invent it",
+          author: "Bibhuti Poudyal",
+          created_at: "20/08/2020",
+          views: 108
+        },
+        {
+          uuid: 1,
+          title: "The best way to predict the way is to invent it",
+          author: "Bibhuti Poudyal",
+          created_at: "20/08/2020",
+          views: 108
+        },
+        {
+          uuid: 1,
+          title: "The best way to predict the way is to invent it",
+          author: "Bibhuti Poudyal",
+          created_at: "20/08/2020",
+          views: 108
+        },
+        {
+          uuid: 1,
+          title: "The best way to predict the way is to invent it",
+          author: "Bibhuti Poudyal",
+          created_at: "20/08/2020",
+          views: 108
+        },
+        {
+          uuid: 1,
+          title: "The best way to predict the way is to invent it",
+          author: "Bibhuti Poudyal",
+          created_at: "20/08/2020",
+          views: 108
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/styles/pages/_dashboard_blogs.scss";
+</style>
---
+++ pages/dashboard/create-blog.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="create-blog dashboard-content d-flex">
+    <div class="post-contents flex-fill">
+      <div class="toolbar">
+        <h1 class="title">Write Blog Post</h1>
+        <div class="toolbar-controls">
+          <button type="button" class="btn btn-primary mb-2">Publish</button>
+          <button type="button" class="btn btn-link mb-2">Preview</button>
+          <button type="button" class="btn btn-link mb-2">Close</button>
+        </div>
+      </div>
+
+      <form class="post-form pr-4">
+        <div class="form-group">
+          <label for="post-title">Post Title</label>
+          <input type="text" class="form-control" id="post-title" placeholder />
+        </div>
+
+        <div class="form-group">
+          <label for="post-tags">Tags</label>
+          <div class="post-tags form-control">
+            <div class="selected-tag" v-for="tag in tags" :key="tag.id">
+              {{tag.title}}
+              <i class="fal fa-times" @click="removeTag(tag)"></i>
+            </div>
+          </div>
+          <div class="tags-container d-flex">
+            <div
+              class="tag"
+              v-for="tag in defaultTags"
+              :key="tag.id"
+              @click="addTag(tag)"
+            >{{tag.title}}</div>
+          </div>
+        </div>
+
+        <div class="form-group">
+          <label for="post-text"></label>
+          <textarea class="form-control" rows="10"></textarea>
+        </div>
+      </form>
+    </div>
+
+    <div class="post-settings">
+      <h1 class="title">Post Settings</h1>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  layout: "dashboard",
+  components: {},
+  data() {
+    return {
+      tags: [],
+      defaultTags: [
+        {
+          id: 1,
+          title: "Education"
+        },
+        {
+          id: 2,
+          title: "Nepal"
+        },
+        {
+          id: 3,
+          title: "Computing"
+        }
+      ],
+      mode: "Create"
+    };
+  },
+
+  methods: {
+    autosave() {},
+
+    addTag(tag) {
+      this.tags.push(tag);
+      this.defaultTags.splice(this.defaultTags.indexOf(tag), 1);
+    },
+
+    removeTag(tag) {
+      this.tags.splice(this.tags.indexOf(tag), 1);
+      this.defaultTags.push(tag);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/styles/pages/dashboard_create_blog.scss";
+</style>
