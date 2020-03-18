b4392a25703164526064a08e8111dabbbce38391
Bibhuti <bibhutipd@gmail.com>
2020-03-18 19:34:09 +0545

    Links contact form with API

--- components/ContactInfo.vue
+++ components/ContactInfo.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="contact-info-wrapper">
-    <div class="container">
+    <div :class="{'container': !isMobile, 'container-fluid': isMobile}">
       <h1 class="page-title">CONTACT US</h1>
       <div class="contact-info-container">
         <div class="contact-card">
@@ -79,11 +79,23 @@
           <form @submit="handleFormSubmit">
             <div class="form-group">
               <label for="fullname">Your Full Name</label>
-              <input type="text" class="form-control" id="fullname" aria-describedby="fullname" />
+              <input
+                type="text"
+                class="form-control"
+                id="fullname"
+                aria-describedby="fullname"
+                v-model="formData.name"
+              />
             </div>
             <div class="form-group">
               <label for="email">Your Email Address</label>
-              <input type="email" class="form-control" id="email" aria-describedby="email" />
+              <input
+                type="email"
+                class="form-control"
+                id="email"
+                aria-describedby="email"
+                v-model="formData.email"
+              />
             </div>
             <div class="form-group">
               <label for="concernedtopic">Your Concerned Topic</label>
@@ -92,14 +104,17 @@
                 class="form-control"
                 id="concernedtopic"
                 aria-describedby="concernedtopic"
+                v-model="formData.topic"
               />
             </div>
             <div class="form-group">
               <label for="message">Message</label>
-              <textarea class="form-control" id="message" rows="5"></textarea>
+              <textarea class="form-control" id="message" rows="5" v-model="formData.message"></textarea>
             </div>
 
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" class="btn btn-primary" :disabled="!valid || loading">
+              <Spinner v-if="loading" />Submit
+            </button>
           </form>
         </div>
       </div>
@@ -108,14 +123,71 @@
 </template>
 
 <script>
+import Spinner from "~/components/Spinner";
+import Helper from "~/helpers/common";
+
 export default {
   name: "HomeAbout",
+  components: { Spinner },
   data() {
-    return {};
+    return {
+      isMobile: false,
+      loading: false,
+      formData: {
+        name: null,
+        email: null,
+        topic: null,
+        message: null
+      }
+    };
   },
+
+  mounted() {
+    if (process.client) {
+      $(window).resize(() => {
+        this.checkIfMobile();
+      });
+      this.checkIfMobile();
+    }
+  },
+
   methods: {
     handleFormSubmit(e) {
       e.preventDefault();
+      this.loading = true;
+      this.$axios
+        .post("/mail", this.formData)
+        .then(response => {
+          this.$toast.show(response.data.message);
+          this.loading = false;
+          this.formData = {
+            name: null,
+            email: null,
+            topic: null,
+            message: null
+          };
+        })
+        .catch(error => {
+          Helper.displayError(this.$toast, error);
+          this.loading = false;
+        });
+    },
+
+    checkIfMobile() {
+      this.isMobile = $(window).width() < 768;
+    }
+  },
+
+  computed: {
+    valid() {
+      return (
+        this.formData.name &&
+        this.formData.name.length > 1 &&
+        this.formData.email &&
+        this.formData.email.length > 1 &&
+        this.formData.topic &&
+        this.formData.topic.length > 1
+      );
     }
   }
 };
