97acd6c4cde8a5a9bf206c325dbe3e69337c2ff3
Bibhuti <bibhutipd@gmail.com>
2020-03-29 13:46:03 +0545

    Adds reset password feature

--- pages/Login.vue
+++ pages/Login.vue
@@ -4,45 +4,78 @@
       <div class="text-center">
         <img src="/images/logo-nav.png" class="logo" />
       </div>
-      <div class="form-group mb-4">
-        <label for="email">Email Address</label>
-        <input
-          type="email"
-          class="form-control"
-          id="email"
-          aria-describedby="emailHelp"
-          placeholder="Enter email"
-          @keyup.enter="handleSubmit()"
-          v-model="email"
-        />
+
+      <div v-if="view === 'login'">
+        <div class="form-group mb-4">
+          <label for="email">Email Address</label>
+          <input
+            type="email"
+            class="form-control"
+            id="email"
+            aria-describedby="emailHelp"
+            placeholder="Enter email"
+            @keyup.enter="handleSubmit()"
+            v-model="email"
+          />
+        </div>
+        <div class="form-group mb-4">
+          <label for="password">Password</label>
+          <input
+            type="password"
+            class="form-control"
+            id="password"
+            placeholder="Password"
+            @keyup.enter="handleSubmit()"
+            v-model="password"
+          />
+        </div>
+        <div class="login-options d-flex justify-content-between mb-4">
+          <div class="form-check align-self-center">
+            <input class="form-check-input" type="checkbox" value id="defaultCheck1" />
+            <label class="form-check-label" for="defaultCheck1">Keep me logged in</label>
+          </div>
+
+          <button
+            class="btn btn-link align-self-center"
+            @click="view='reset-password'"
+          >Forgot Password ?</button>
+        </div>
+        <div class="text-center">
+          <button
+            class="btn btn-primary btn-block"
+            @click="handleSubmit()"
+            :disabled="email.length <= 0 || password.length <= 0 || loginLoading"
+          >
+            <Spinner v-if="loginLoading" />Login
+          </button>
+        </div>
       </div>
-      <div class="form-group mb-4">
-        <label for="password">Password</label>
-        <input
-          type="password"
-          class="form-control"
-          id="password"
-          placeholder="Password"
-          @keyup.enter="handleSubmit()"
-          v-model="password"
-        />
-      </div>
-      <div class="login-options d-flex justify-content-between mb-4">
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" value id="defaultCheck1" />
-          <label class="form-check-label" for="defaultCheck1">Keep me logged in</label>
+
+      <div v-else-if="view === 'reset-password'">
+        <div class="form-group mb-2">
+          <label for="email">Email Address</label>
+          <input
+            type="email"
+            class="form-control"
+            id="email"
+            aria-describedby="emailHelp"
+            placeholder="Enter email"
+            @keyup.enter="handleResetPasswordSubmit()"
+            v-model="resetEmail"
+          />
         </div>
 
-        <nuxt-link to="/login">Forgot Password ?</nuxt-link>
-      </div>
-      <div class="text-center">
-        <button
-          class="btn btn-primary btn-block"
-          @click="handleSubmit()"
-          :disabled="email.length <= 0 || password.length <= 0 || loginLoading"
-        >
-          <Spinner v-if="loginLoading" />Login
-        </button>
+        <button class="btn btn-link mb-4 text-align-left pl-0" @click="view='login'">Go back to login</button>
+
+        <div class="text-center">
+          <button
+            class="btn btn-primary btn-block"
+            @click="handleResetPasswordSubmit()"
+            :disabled="(resetEmail && resetEmail.length <= 0) || resetPasswordLoading"
+          >
+            <Spinner v-if="resetPasswordLoading" />Send Reset Password Link
+          </button>
+        </div>
       </div>
     </div>
   </div>
@@ -58,7 +91,10 @@ export default {
     return {
       email: "",
       password: "",
-      loginLoading: false
+      loginLoading: false,
+      resetPasswordLoading: false,
+      resetEmail: null,
+      view: "login" // login, reset-password
     };
   },
 
@@ -85,6 +121,19 @@ export default {
         this.$toast.show("Invalid credentials");
         this.loginLoading = false;
       }
+    },
+
+    handleResetPasswordSubmit() {
+      this.resetPasswordLoading = true;
+      this.$axios
+        .post(`/password-reset/request`, {
+          email: this.resetEmail
+        })
+        .then(response => {
+          this.$toast.show("Reset link successfully sent to your email.");
+          this.resetPasswordLoading = false;
+          this.view = login;
+        });
     }
   },
   created() {}
---
+++ pages/reset-password/_reset_token.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="reset-token-page">
+    <div class="reset-card">
+      <div class="text-center">
+        <img src="/images/logo-nav.png" class="logo" />
+      </div>
+
+      <div>
+        <h2 class="text-center mt-4">Password Reset</h2>
+        <br />
+        <div class="form-group mb-4 mt-4">
+          <label for="email">New Password</label>
+          <input
+            type="password"
+            class="form-control"
+            id="password"
+            placeholder="Password"
+            @keyup.enter="handleResetPasswordSubmit()"
+            v-model="password"
+          />
+        </div>
+
+        <div class="form-group mb-4">
+          <label for="email">Password Confirmation</label>
+          <input
+            type="password"
+            class="form-control"
+            id="password-confirmation"
+            placeholder="Password Confirmation"
+            @keyup.enter="handleResetPasswordSubmit()"
+            v-model="passwordConfirmation"
+          />
+        </div>
+
+        <div class="text-center">
+          <button class="btn btn-primary" @click="handleResetPasswordSubmit()" :disabled="!isValid">
+            <Spinner v-if="resetLoading" />Submit
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Spinner from "~/components/Spinner";
+import Helper from "~/helpers/common";
+
+export default {
+  auth: false,
+  components: { Spinner },
+  data() {
+    return {
+      resetToken: this.$route.params.reset_token,
+      password: "",
+      passwordConfirmation: "",
+      resetLoading: false
+    };
+  },
+
+  //   async asyncData({ $axios, params, error }) {
+  //     try {
+  //       const response = await $axios.post(
+  //         `/password-reset/${params.reset_token}`
+  //       );
+  //     } catch (e) {
+  //       error({ statusCode: 404 });
+  //     }
+  //   },
+
+  methods: {
+    handleResetPasswordSubmit() {
+      this.resetLoading = true;
+      this.$axios
+        .post(`/password-reset`, {
+          token: this.resetToken,
+          password: this.password
+        })
+        .then(response => {
+          this.$toast.show("Password reset successfull");
+          this.resetLoading = false;
+        })
+        .catch(error => {
+          Helper.displayError(this.$toast, error);
+          this.resetLoading = false;
+        });
+    }
+  },
+
+  mounted() {
+    alert(this.resetToken);
+  },
+
+  computed: {
+    isValid() {
+      return (
+        this.password &&
+        this.password.length > 0 &&
+        this.passwordConfirmation &&
+        this.passwordConfirmation.length > 0 &&
+        this.password === this.passwordConfirmation &&
+        !this.resetLoading
+      );
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/styles/pages/reset_token.scss";
+</style>
